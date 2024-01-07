843a165482da9ebe90f6dde9471c5bdf675067f5
Mahmod Hassan <mahmodhasan7788@gmail.com>
2024-01-07 14:22:53 +0600

    get optimize images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import croppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
 interface Props {
@@ -8,7 +9,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={croppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
---
+++ src/services/image-url.ts
@@ -0,0 +1,8 @@
+// to optimize image we added this string
+// 'crop/600/400
+const croppedImageUrl = (url: string) => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+export default croppedImageUrl;
