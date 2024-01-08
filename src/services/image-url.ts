3f7e3954bb2f3bf447e9eb264e7f65be35394ab4
Mahmod Hassan <mahmodhasan7788@gmail.com>
2024-01-08 12:21:19 +0600

    sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
 // to optimize image we added this string
 // 'crop/600/400
 const croppedImageUrl = (url: string) => {
+  if (!url) return "";
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
