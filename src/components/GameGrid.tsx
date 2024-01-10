5c74aa31ae2a3f4af95ff47dc6e46c6900d721cf
Mahmod Hassan <mahmodhasan7788@gmail.com>
2024-01-10 21:43:14 +0600

    Refactoring - remove duplicate interfaces

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 const GameGrid = ({ gameQuery }: Props) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
-  console.log(games);
+
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   if (error) return <Text>{error.message}</Text>;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,13 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-
 import apiClient, { FetchResponse } from "../services/api-client";
-import { Platform } from "./useGames";
 // import platforms from "../data/platforms";
+
+export interface Platform {
+  id: string;
+  name: string;
+  slug: string;
+}
 
 const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
-    queryFn: async () =>
+    queryFn: () =>
       apiClient
         .get<FetchResponse<Platform>>("/platforms/lists/parents")
         .then((res) => res.data),
