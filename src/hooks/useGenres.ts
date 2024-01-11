bf522dde08669c67076ae7ab1e3c0bd92e29eb82
Mahmod Hassan <mahmodhasan7788@gmail.com>
2024-01-11 21:07:15 +0600

    Refector- simplify time calculation

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-// import genres from "../data/genres";
+import ms from "ms";
+import genres from "../data/genres";
 import APIClient from "../services/api-client";
-
 export interface Genre {
   id: number;
   name: string;
@@ -13,8 +13,8 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
-    // initialData: { count: genres.length, results: genres },
+    staleTime: ms("24h"),
+    initialData: genres,
   });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
+import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
 
 export interface Platform {
@@ -13,7 +15,8 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
+    initialData: platforms,
   });
 
 export default usePlatforms;
