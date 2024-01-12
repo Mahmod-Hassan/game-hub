214d4248ddc90836997689ccd8a8e4b2ea48a21a
Mahmod Hassan <mahmodhasan7788@gmail.com>
2024-01-12 21:38:45 +0600

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
@@ -6,6 +8,8 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  publishers: Publisher[];
+  genres: Genre[];
   rating_top: number;
   slug: string;
   description_raw: string;
