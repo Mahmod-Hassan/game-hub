c2c5d63776dd049ff32ac57166056c73da7b3abf
Mahmod Hassan <mahmodhasan7788@gmail.com>
2024-01-12 12:41:41 +0600

    handle errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import router from "./routes.tsx";
 import theme from "./theme.ts";
 
 const queryClient = new QueryClient();
@@ -13,8 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools />
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
---
+++ src/pages/GameDetailsPage.tsx
@@ -0,0 +1,9 @@
+const GameDetailsPage = () => {
+  return (
+    <div>
+      <h1>game details page</h1>
+    </div>
+  );
+};
+
+export default GameDetailsPage;
